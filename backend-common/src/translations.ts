5d609386b0ea1ee6a098f772337a129c04a0e363
Lorenzo <hello@lorenzo0111.me>
2024-07-06 20:06:47 +0200

    Added language server support to backend & deps update

--- backend-common/src/translations.ts
+++ backend-common/src/translations.ts
@@ -1,19 +1,27 @@
-import { readFileSync, readdirSync } from "node:fs";
+import axios from "axios";
 
 const locales = new Map<string, any>();
-readdirSync("../translations").forEach((locale) => {
-  locales.set(
-    locale,
-    JSON.parse(
-      readFileSync(`../translations/${locale}/translation.json`, "utf-8")
+if (locales.size === 0) {
+  axios
+    .get(`${process.env.TRANSLATIONS_URL}/api/list`)
+    .then((res) => res.data)
+    .then(async (list: string[]) =>
+      Promise.all(
+        list.map(async (locale: string) => {
+          return await axios
+            .get(`${process.env.TRANSLATIONS_URL}/api/download/${locale}`)
+            .then((res) => res.data)
+            .then((translation) => locales.set(locale, translation));
+        })
+      )
     )
-  );
-});
-
-console.log(
-  `[i18n] Loaded locales (${Array.from(locales.keys()).length}):`,
-  Array.from(locales.keys()).join(", ")
-);
+    .then(() =>
+      console.log(
+        `[i18n] Loaded locales (${Array.from(locales.keys()).length}):`,
+        Array.from(locales.keys()).join(", ")
+      )
+    );
+}
 
 export function isLanguageSupported(language: string) {
   return locales.has(language);
